69e7befdf0c9129d23f8af7a49f84f0d39cf153a
lzhabo <lidia.zhabo99@gmail.com>
2022-10-31 14:03:58 +0000

    remove unused scripts

--- scripts/removeHomepageFromPackage.js
+++
@@ -1,5 +0,0 @@
-console.log("ok");
-const fs = require("fs");
-const packageJson = require("../package.json");
-delete packageJson.homepage;
-fs.writeFileSync("package.json", JSON.stringify(packageJson));
